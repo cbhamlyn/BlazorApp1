f8d7dd28287a75477d6b0612048d097d244fedcb
Charles B Hamlyn <cbhamlyn@gmail.com>
2023-05-10 19:08:58 -0400

    Initial Commit

---
+++ BlazorApp1/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1
+{
+    public class TodoItem
+    {
+        // Get or Set the Title of the Todo item (the "?" in "string?" means the string can be null!)
+        public string? Title { get; set; }
+        // Get or Set the IsDone flag for this specific Todo item.
+        public bool IsDone { get; set; } = false;
+    }
+}
